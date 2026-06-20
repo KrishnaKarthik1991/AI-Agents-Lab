c34a824ab1591e822cfa33aea522cbdcd3152cae
Krishna karthik <krishnakarthik@Krishnas-MacBook-Air.local>
2026-06-20 19:39:54 +0530

    Completed Day 7

---
+++ main.py
@@ -0,0 +1,10 @@
+feature = {
+    "name": "payments",
+    "priority":"medium"
+}
+print("feature:",feature["name"])
+if feature ["priority"] == "High":
+    print("Build last")
+task = ("Analysis","Design","Develop")
+for task in task:
+    print("task:", task)
