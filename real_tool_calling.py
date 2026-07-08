033baa5654447438b2c929dde92523d6a2ecf256
Krishna karthik <krishnakarthik@Krishnas-MacBook-Air.local>
2026-07-08 19:03:10 +0530

    Understanding real function calling

---
+++ real_tool_calling.py
@@ -0,0 +1,7 @@
+def get_weather(city):
+    return f"The current weather in {city} is sunny with a temperature of 25°C."
+def calculate(a,b):
+    return a + b
+def greet(name):
+    return f"Hello, {name}! Welcome to the AI Agents Lab."
+question = input("Enter a question for the AI assistant: ").lower()
