3854c51ea7012898c3c06416c9965207673e3bfb
Krishna karthik <krishnakarthik@Krishnas-MacBook-Air.local>
2026-07-18 12:33:27 +0530

    Upgrade to chat bot

--- weather-agent/app.py
+++ weather-agent/app.py
@@ -1,14 +1,29 @@
 import streamlit as st
+if "messages" not in st.session_state:
+      st.session_state.messages=[]
+for message in st.session_state.messages:
+      with st.chat_message(message["role"]):
+            st.write(message["content"])
 from main import process_question
 
 st.title("🤖 Karthik AI Assistant")
 
-question = st.text_input("Ask me anything")
+question = st.chat_input("Ask me anything")
 
-if st.button("Send"):
-    if question:
-        with st.spinner("Thinking..."):
+if question:
+    st.session_state.messages.append(
+          {
+                "role":"user",
+                "content":question
+          }
+    )
+    with st.spinner("Thinking..."):
             answer = process_question(question)
-
-        st.success("Done!")
-        st.write(answer)
+    st.session_state.messages.append(
+          {
+                "role":"assistant",
+                "content":answer
+          }
+    )
+    st.success("Done!")
+    st.rerun()
